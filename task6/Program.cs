c0fe146e3a44310a1697ceaedb7663984914eb25
MelJelif <nifilim@mail.ru>
2023-03-27 10:56:48 +1000

    Задача6

---
+++ task6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 2;
+if (b == 0)
+   Console.Write("Да");
+else
+   Console.Write("Нет");
