f70ecbe1370a15303f4a5668e492a51b4a7ff5d4
MelJelif <nifilim@mail.ru>
2023-03-27 10:23:29 +1000

    задача 2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+   Console.WriteLine($"Max = {a}");
+else
+   Console.WriteLine($"Max = {b}");
