c40f0e11efec0f382f1d21a704dbc9f5b4b80ff1
MelJelif <nifilim@mail.ru>
2023-03-27 11:28:18 +1000

    Задача8 + фикс

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int b = a % 2;
-if (b == 0)
+if (a % 2 == 0)
    Console.Write("Да");
 else
    Console.Write("Нет");
---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < a + 1)
+    if (i % 2 == 0){
+    // Console.Write($" {i},"); к сожалению не понял как сделать , чтобы после последнего числа не ставилась запятая
+    Console.Write($" {i}");
+    i++;
+    }
+    else
+    i++;
