6618e258c8ccbb4803de59e404325e27b7c8b555
MelJelif <nifilim@mail.ru>
2023-03-27 10:41:04 +1000

    задача 4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-ье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+   if (a > c)
+   Console.WriteLine(a);
+   else
+   Console.WriteLine(c);
+else
+   if (b > c)
+   Console.WriteLine(b);
+   else
+   Console.WriteLine(c);
