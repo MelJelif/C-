7a60d90a4f256149ca4644b3d6c8898f8b656149
MelJelif <nifilim@mail.ru>
2023-03-28 11:09:16 +1000

    задача 15

---
+++ task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите порядковый номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a < 1 || a > 7)
+{
+    Console.WriteLine("Вы допустили ощибку. Нумерация дней идет от 1 до 7.");
+    Console.Write("Введите порядковый номер дня недели: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a > 5)
+   Console.WriteLine($"\n Это Выходной");
+else
+   Console.WriteLine($"\n Это Будний день");
+
