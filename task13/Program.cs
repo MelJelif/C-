941df019d34065ceebd849f438e48f9b2fdf596a
MelJelif <nifilim@mail.ru>
2023-03-28 10:30:22 +1000

    Task13 + debug content for all

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 99){
+   while (a > 999){
+    a /= 10;
+   //  Console.WriteLine(a); для проверки
+   }
+Console.WriteLine($"{a%10}");
+}
+else 
+   Console.WriteLine("третьего числа нет");
