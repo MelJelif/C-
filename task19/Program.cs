2668f53e4308a8c0909aa8dc886c0655bebd76ad
MelJelif <nifilim@mail.ru>
2023-03-31 18:06:23 +1000

    zadacha19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число:");
+string a = Console.ReadLine()!;
+// Console.WriteLine(a);
+while (a.Length != 5)
+{   Console.WriteLine("Выдопустили ошибку");
+    Console.WriteLine("Введите пятизначное число:");
+    a = Console.ReadLine()!;
+}
+//Console.WriteLine(a);
+if (a[0]==a[4] && a[1]==a[3]){
+//    Console.WriteLine("Hell yeah!!! это число палиндром!");
+   Console.WriteLine("Да");
+   }
+else
+    Console.WriteLine("нет");
