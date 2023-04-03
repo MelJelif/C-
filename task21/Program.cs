43165350254a3f1cbb44afe24f4e86b081de372d
MelJelif <meljelif@gmail.com>
2023-04-03 18:49:41 +1000

    task21 rabotaet

--- task21/Program.cs
+++ task21/Program.cs
@@ -1,13 +1,22 @@
-﻿Console.Clear();
-Console.Write("Введите координаты первой точки через запятую:");
-string a = Console.ReadLine()!;
+﻿Console.Write("Введите координаты первой точки через точкус запятой:");
+string[] a = Console.ReadLine()!.Split(';');
+// // foreach (string c in a)
+// {
+//     Console.WriteLine(c);
+// }
+Console.WriteLine(string.Join(",", a));
 Console.Write("Введите координаты второй точки через запятую:");
-string b = Console.ReadLine()!;
-int r1 = (int)Math.Pow(int.Parse(b[0].ToString())-int.Parse(a[0].ToString()),2);
+string[] b = Console.ReadLine()!.Split(';');
+Console.WriteLine(string.Join(",", b));
+// foreach (string c in b)
+// {
+//     Console.WriteLine(c);
+// }
+double r1 = Math.Pow(Convert.ToDouble(b[0]) - Convert.ToDouble(a[0]), 2);
 Console.WriteLine($"r1={r1}");
-int r2 = (int)Math.Pow(int.Parse(b[2].ToString())-int.Parse(a[2].ToString()),2);
+double r2 = Math.Pow(Convert.ToDouble(b[1]) - Convert.ToDouble(a[1]), 2);
 Console.WriteLine($"r2={r2}");
-int r3 = (int)Math.Pow(int.Parse(b[4].ToString())-int.Parse(a[4].ToString()),2);
-Console.WriteLine(r3);
-int rang = (int)Math.Sqrt(r1+ r2+ r3);
-Console.WriteLine(rang);
+double r3 = Math.Pow(Convert.ToDouble(b[2]) - Convert.ToDouble(a[2]), 2);
+Console.WriteLine($"r3={r3}");
+double rang = Math.Sqrt(r1 + r2 + r3);
+Console.WriteLine($"rang={rang}");
