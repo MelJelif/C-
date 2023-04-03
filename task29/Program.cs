563d5fb6c6f40c0033fc35708e0bded12eb0c453
MelJelif <meljelif@gmail.com>
2023-04-03 21:23:39 +1000

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите восемь значений через точкус запятой:");
+string[] a = Console.ReadLine()!.Split(';');
+while (a.Length != 8){
+    Console.WriteLine("Вы допустили ошибку:");
+    Console.WriteLine("Введите восемь значений через точкус запятой:");
+    a = Console.ReadLine()!.Split(';');
+}
+Console.WriteLine($"[{string.Join(",", a)}]");
